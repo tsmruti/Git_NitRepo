7af7d06b4dfd0f3c192ede623f62417fc55c502f
GitNitMukesh <mukeshsatapathy@gmail.com>
2017-05-02 00:05:33 +0530

    commit before staching

--- Sample_proj1/src/com/nt/jdbc/sample.java
+++ Sample_proj1/src/com/nt/jdbc/sample.java
@@ -3,7 +3,7 @@ package com.nt.jdbc;
 public class sample {
 	public static void main(String[] args) {
 		System.out.println("hi");
-		System.out.println("hello");
+		System.out.println("hello1223");
 
 	}
 }
